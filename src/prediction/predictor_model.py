76bd1a2583d769faf48a623a8be653c4e3a9d8d2
moo3030 <mohamedabdelhamid3030@gmail.com>
2023-12-17 08:31:10 +0200

    refactor

--- src/prediction/predictor_model.py
+++ src/prediction/predictor_model.py
@@ -27,12 +27,16 @@ class Forecaster:
     def __init__(
         self,
         data_schema: ForecastingSchema,
+        history_forecast_ratio: int = None,
+        lags_forecast_ratio: int = None,
         n_estimators: int = 100,
         boosting_type: Optional[str] = "gbdt",
         num_leaves: Optional[int] = 31,
         max_depth: Optional[int] = -1,
         learning_rate: Optional[float] = 0.1,
-        lags: Union[int, List[int]] = 7,
+        lags: Union[int, List[int]] = 20,
+        use_exogenous: bool = True,
+        verbose: int = 1,
         random_state: int = 0,
     ):
         """Construct a new LGBM Forecaster
@@ -40,6 +44,17 @@ class Forecaster:
         Args:
 
             data_schema (ForecastingSchema): Schema of the data used for training.
+
+            history_forecast_ratio (int):
+                Sets the history length depending on the forecast horizon.
+                For example, if the forecast horizon is 20 and the history_forecast_ratio is 10,
+                history length will be 20*10 = 200 samples.
+
+            lags_forecast_ratio (int):
+                Sets the lags parameters depending on the forecast horizon.
+                lags = forecast horizon * lags_forecast_ratio
+                This parameters overides lags parameters.
+
 
             n_estimators (int): Number of boosted trees to fit.
 
@@ -60,36 +75,93 @@ class Forecaster:
                 - int: include lags from 1 to lags (included).
                 - list, 1d numpy ndarray or range: include only lags present in lags, all elements must be int.
 
+            use_exogenous (bool):
+                If true, uses covariates in training.
+
+            verbose (int):
+                controls the level of LightGBM's verbosity
+
             random_state (int): Sets the underlying random seed at model initialization time.
         """
         self.n_estimators = n_estimators
-        self.leaarning_rate = learning_rate
+        self.learning_rate = learning_rate
         self.boosting_type = boosting_type
         self.num_leaves = num_leaves
         self.max_depth = max_depth
         self.random_state = random_state
+        self.use_exogenous = use_exogenous
+        self.verbose = verbose
         self.lags = lags
         self._is_trained = False
         self.models = {}
         self.data_schema = data_schema
         self.end_index = {}
+        self.history_length = None
+
+        if history_forecast_ratio:
+            self.history_length = (
+                self.data_schema.forecast_length * history_forecast_ratio
+            )
+        if lags_forecast_ratio:
+            lags = self.data_schema.forecast_length * lags_forecast_ratio
+            self.lags = lags
+
+    def _add_future_covariates_from_date(
+        self,
+        history: pd.DataFrame,
+        data_schema: ForecastingSchema,
+        is_training: bool = True,
+    ) -> pd.DataFrame:
+        """
+        Creates future covariates from the data column.
+
+        Args:
+            history (pd.DataFrame):
+                The data to create the covariates on.
+
+            data_schema (ForecastingSchema):
+                Schema of the data.
+
+            is_training (bool):
+                Set to true if the process is to be done on the training data.
+
+        Returns (pd.DataFrame): The processed dataframe.
+        """
+
+        future_covariates_names = data_schema.future_covariates
+        if data_schema.time_col_dtype in ["DATE", "DATETIME"]:
+            date_col = pd.to_datetime(history[data_schema.time_col])
+            year_col = date_col.dt.year
+            month_col = date_col.dt.month
+            year_col_name = f"{data_schema.time_col}_year"
+            month_col_name = f"{data_schema.time_col}_month"
+            history[year_col_name] = year_col
+            history[month_col_name] = month_col
+            if is_training:
+                future_covariates_names += [year_col_name, month_col_name]
+
+        return history, future_covariates_names
 
     def fit(
         self,
         history: pd.DataFrame,
         data_schema: ForecastingSchema,
-        history_length: int = None,
     ) -> None:
         """Fit the Forecaster to the training data.
-        A separate LGBM model is fit to each series that is contained
+        A separate XGBoost model is fit to each series that is contained
         in the data.
 
         Args:
             history (pandas.DataFrame): The features of the training data.
             data_schema (ForecastingSchema): The schema of the training data.
-            history_length (int): The length of the series used for training.
         """
         np.random.seed(self.random_state)
+        (
+            history,
+            future_covariates,
+        ) = self._add_future_covariates_from_date(
+            history=history, data_schema=data_schema, is_training=True
+        )
         groups_by_ids = history.groupby(data_schema.id_col)
         all_ids = list(groups_by_ids.groups.keys())
         all_series = [
@@ -100,9 +172,14 @@ class Forecaster:
         self.models = {}
 
         for id, series in zip(all_ids, all_series):
-            if history_length:
-                series = series[-history_length:]
-            model = self._fit_on_series(history=series, data_schema=data_schema, id=id)
+            if self.history_length:
+                series = series[-self.history_length :]
+            model = self._fit_on_series(
+                history=series,
+                data_schema=data_schema,
+                id=id,
+                future_covariates=future_covariates,
+            )
             self.models[id] = model
 
         self.all_ids = all_ids
@@ -110,27 +187,31 @@ class Forecaster:
         self.data_schema = data_schema
 
     def _fit_on_series(
-        self, history: pd.DataFrame, data_schema: ForecastingSchema, id: int
+        self,
+        history: pd.DataFrame,
+        data_schema: ForecastingSchema,
+        id: int,
+        future_covariates: List = None,
     ):
-        """Fit LGBM model to given individual series of data"""
+        """Fit XGBoost model to given individual series of data"""
         model = LGBMRegressor(
             n_estimators=self.n_estimators,
-            learning_rate=self.leaarning_rate,
+            learning_rate=self.learning_rate,
             num_leaves=self.num_leaves,
             boosting_type=self.boosting_type,
-            random_state=self.random_state,
             max_depth=self.max_depth,
-            verbose=-1,
+            random_state=self.random_state,
+            verbose=self.verbose,
         )
         forecaster = ForecasterAutoreg(regressor=model, lags=self.lags)
 
-        covariates = data_schema.future_covariates
+        covariates = future_covariates
 
         history.index = pd.RangeIndex(start=0, stop=len(history))
 
         self.end_index[id] = len(history)
         exog = None
-        if covariates:
+        if covariates and self.use_exogenous:
             exog = history[covariates]
 
         forecaster.fit(y=history[data_schema.target], exog=exog)
@@ -149,6 +230,10 @@ class Forecaster:
         if not self._is_trained:
             raise NotFittedError("Model is not fitted yet.")
 
+        test_data, future_covariates = self._add_future_covariates_from_date(
+            history=test_data, data_schema=self.data_schema, is_training=False
+        )
+
         groups_by_ids = test_data.groupby(self.data_schema.id_col)
         all_series = [
             groups_by_ids.get_group(id_).drop(columns=self.data_schema.id_col)
@@ -158,7 +243,9 @@ class Forecaster:
         all_forecasts = []
         for id_, series_df in zip(self.all_ids, all_series):
             forecast = self._predict_on_series(
-                key_and_future_df=(id_, series_df), id=id_
+                key_and_future_df=(id_, series_df),
+                id=id_,
+                future_covariates=future_covariates,
             )
             forecast.insert(0, self.data_schema.id_col, id_)
             all_forecasts.append(forecast)
@@ -171,15 +258,15 @@ class Forecaster:
         )
         return all_forecasts
 
-    def _predict_on_series(self, key_and_future_df, id):
+    def _predict_on_series(self, key_and_future_df, id, future_covariates):
         """Make forecast on given individual series of data"""
         key, future_df = key_and_future_df
 
         start = self.end_index[id]
         future_df.index = pd.RangeIndex(start=start, stop=start + len(future_df))
         exog = None
-        covariates = self.data_schema.future_covariates
-        if covariates:
+        covariates = future_covariates
+        if covariates and self.use_exogenous:
             exog = future_df[covariates]
 
         if self.models.get(key) is not None:
@@ -238,18 +325,9 @@ def train_predictor_model(
     Returns:
         'Forecaster': The Forecaster model
     """
-    history_length = None
-    history_forecast_ratio = hyperparameters.get("history_forecast_ratio")
-    if history_forecast_ratio:
-        history_length = data_schema.forecast_length * history_forecast_ratio
-    if "history_forecast_ratio" in hyperparameters:
-        hyperparameters.pop("history_forecast_ratio")
 
-    model = Forecaster(
-        data_schema=data_schema,
-        **hyperparameters,
-    )
-    model.fit(history=history, data_schema=data_schema, history_length=history_length)
+    model = Forecaster(data_schema=data_schema, **hyperparameters)
+    model.fit(history=history, data_schema=data_schema)
     return model
 
 
